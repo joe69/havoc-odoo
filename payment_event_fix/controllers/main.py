4f479d59c17e7f799a0b70d5bb6ff7f31290b9d5
Thomas Entlicher <thomas@wegot.vision>
2025-11-25 14:03:26 +0000

    Update

--- payment_event_fix/controllers/main.py
+++ payment_event_fix/controllers/main.py
@@ -4,27 +4,13 @@ from odoo.http import request, route
 
 class WebsiteSaleForceBilling(WebsiteSale):
 
-    @route(['/shop/checkout'], type='http', auth='public', website=True)
-    def checkout(self, **post):
+    @route(['/shop/address'], type='http', auth='public', website=True, sitemap=False)
+    def address(self, **kw):
         """
-        Verhindert, dass Odoo den Rechnungs-/Adress-Schritt überspringt.
-        Funktioniert ohne request.website.sale_get_order().
+        Verhindert das Überspringen des Adress-Schritts bei Events.
+        Setzt try_skip_step=true auf false.
         """
-
-        # Sale Order aus der Session holen (Standard bei Website Sale)
-        sale_order_id = request.session.get('sale_order_id')
-        order = request.env['sale.order'].sudo().browse(sale_order_id) if sale_order_id else request.env['sale.order']
-
-        # Wenn kein Schritt übergeben wurde, mit 'address' starten
-        if not post.get('step'):
-            post['step'] = 'address'
-
-        # Wenn Odoo direkt zu payment/confirm will, prüfen wir, ob eine Rechnungsadresse sauber gesetzt ist
-        if post.get('step') in ('payment', 'confirm'):
-            partner = getattr(order, 'partner_invoice_id', False)
-            # Hier kannst du definieren, was "vollständige" Rechnungsadresse heißt
-            if not partner or not partner.street or not partner.country_id:
-                # zurück zum Adress-Schritt zwingen
-                post['step'] = 'address'
-
-        return super().checkout(**post)
+        # Parameter zwingen auf false
+        kw['try_skip_step'] = False
+        
+        return super().address(**kw)
