5b7f03ab11fc2072e6089297a9ce4db3d0df6fda
Thomas Entlicher <thomas@wegot.vision>
2025-11-25 14:37:30 +0000

    Update

--- payment_event_fix/models/payment_transaction.py
+++ payment_event_fix/models/payment_transaction.py
@@ -4,41 +4,40 @@ from odoo import models, api
 class PaymentTransaction(models.Model):
     _inherit = 'payment.transaction'
 
+    def _ensure_partner_country(self):
+        """
+        Stellt sicher, dass Partner IMMER AT als Land hat
+        """
+        if self.partner_id and not self.partner_id.country_id:
+            austria = self.env['res.country'].sudo().search([('code', '=', 'AT')], limit=1)
+            if austria:
+                self.partner_id.sudo().write({'country_id': austria.id})
+
     @api.model
     def create(self, vals):
         """
-        Stellt sicher, dass Partner IMMER ein Land hat bevor PayPal-Transaction erstellt wird
+        AT setzen BEVOR Transaction erstellt wird
         """
-        # Partner aus vals oder sale_order holen
         partner_id = vals.get('partner_id')
-        sale_order_id = vals.get('sale_order_ids')
-        
         if partner_id:
             partner = self.env['res.partner'].sudo().browse(partner_id)
-        elif sale_order_id:
-            order = self.env['sale.order'].sudo().browse(sale_order_id[0][2][0] if sale_order_id else False)
-            partner = order.partner_invoice_id or order.partner_id if order else False
-        else:
-            partner = False
-        
-        # AT setzen falls kein Land
-        if partner and not partner.country_id:
-            austria = self.env['res.country'].sudo().search([('code', '=', 'AT')], limit=1)
-            if austria:
-                partner.sudo().write({'country_id': austria.id})
+            if not partner.country_id:
+                austria = self.env['res.country'].sudo().search([('code', '=', 'AT')], limit=1)
+                if austria:
+                    partner.write({'country_id': austria.id})
         
         return super().create(vals)
 
     def _get_specific_rendering_values(self, processing_values):
         """
-        Wird direkt vor PayPal-API-Call aufgerufen
+        AT setzen VOR jedem Payment Provider Call (PayPal, Stripe, etc.)
         """
-        res = super()._get_specific_rendering_values(processing_values)
-        
-        # Partner-Land nochmal prüfen
-        if self.partner_id and not self.partner_id.country_id:
-            austria = self.env['res.country'].sudo().search([('code', '=', 'AT')], limit=1)
-            if austria:
-                self.partner_id.sudo().write({'country_id': austria.id})
-        
-        return res
+        self._ensure_partner_country()
+        return super()._get_specific_rendering_values(processing_values)
+
+    def _send_payment_request(self):
+        """
+        AT setzen VOR Payment Request
+        """
+        self._ensure_partner_country()
+        return super()._send_payment_request()
